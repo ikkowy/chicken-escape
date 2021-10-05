7f9d056e0b841c872c16ec1bda5f1b61537374b8
Vincent Westenberg <vincent.westenberg@posteo.de>
2021-10-05 10:09:28 +0200

    Create empty window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,34 @@
 /* Copyright (C) 2021 Chicken Escape authors */
 
+#include <SDL.h>
+
 int main()
 {
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window = SDL_CreateWindow(
+                             "Chicken Escape",
+                             SDL_WINDOWPOS_CENTERED,
+                             SDL_WINDOWPOS_CENTERED,
+                             800, 600, 0);
+
+    bool quit {false};
+    SDL_Event event;
+
+    while (!quit)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+                case SDL_QUIT:
+                    quit = true;
+                    break;
+            }
+        }
+    }
+
+    SDL_Quit();
+
     return 0;
 }
