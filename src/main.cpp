8f6279e248e33d695d2534b67bda0988261bd462
Vincent Westenberg <vincent.westenberg@posteo.de>
2021-10-05 15:39:11 +0200

    Render chicken bitmap

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,12 @@ int main()
                              SDL_WINDOWPOS_CENTERED,
                              800, 600, 0);
 
+    SDL_Surface *windowSurface = SDL_GetWindowSurface(window);
+
+    SDL_Surface *chickenSurface = SDL_LoadBMP("chicken.bmp");
+
     bool quit {false};
+
     SDL_Event event;
 
     while (!quit)
@@ -26,7 +31,17 @@ int main()
                     break;
             }
         }
+
+        SDL_BlitSurface(chickenSurface, NULL, windowSurface, NULL);
+
+        SDL_UpdateWindowSurface(window);
     }
+
+    SDL_FreeSurface(windowSurface);
+
+    SDL_FreeSurface(chickenSurface);
+
+    SDL_DestroyWindow(window);
 
     SDL_Quit();
 
